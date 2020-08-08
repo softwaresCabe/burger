40041f8da3337677d3eec47475a624f0c6834552
nivek26 <nivek2696@gmail.com>
2020-08-08 03:14:48 -0700

    front end improvements 2

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static('views/images'));
+
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
